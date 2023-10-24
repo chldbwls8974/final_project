3573c94194eef15ea9042bd7bf7cc50c618006cd
ekzot1212 <ekzot1212@gmail.com>
2023-10-24 14:04:46 +0900

    Update AccountController.java

--- src/main/java/kr/kh/final_project/controller/AccountController.java
+++ src/main/java/kr/kh/final_project/controller/AccountController.java
@@ -31,9 +31,7 @@ public class AccountController {
 		//세션에서 유저정보 가져오기 코드 추가
 		MemberVO user = new MemberVO(1,"test", "test", "홍길동", "길동이", 2 , "01012341234" , "", "", "" , "USER", 0 , "", "실버", 0, 0, 0);
 			
-		// "1"은 유저 번호임
-//				AccountVO account = new AccountVO(1, "KB국민은행", "홍길동", "032744567521231");
-		//account의 예금주명은 회원 닉네임으로 받아오기
+		//화면에서 account의 예금주명은 회원 이름으로 받아오기
 		if(accountService.insertAccount(account, user)) {
 			msg = "계좌등록에 성공하였습니다.";
 			url = "/";
