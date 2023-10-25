abb8695add8884b54c84004b9be89927382197db
kjy0411 <134986647+kjy0411@users.noreply.github.com>
2023-10-25 11:48:46 +0900

    경기 등록 이벤트 완료

---
+++ db/event_kjy.sql
@@ -0,0 +1,22 @@
+SET GLOBAL event_scheduler = ON;
+
+USE FUTSAL;
+
+DELIMITER $$
+CREATE EVENT IF NOT EXISTS insert_match_event
+ON SCHEDULE EVERY 1 day
+STARTS TIMESTAMP(CURRENT_DATE, '00:00:00')
+DO
+BEGIN
+    INSERT INTO futsal.match (mt_ga_date, mt_st_num, mt_ti_num, mt_personnel)
+    SELECT
+		date(adddate(now(), INTERVAL 21 DAY)), sc_st_num, sc_ti_num, sc_personnel
+    FROM
+		futsal.schedule
+			join
+		futsal.time on sc_ti_num = ti_num
+    WHERE ti_day = (SELECT SUBSTR('일월화수목금토', DAYOFWEEK(now()), 1));
+END;
+$$
+DELIMITER ;
+
