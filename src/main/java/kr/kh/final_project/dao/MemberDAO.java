7481b620aef412d69f4095d53f6005b9c8cd273b
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-10-27 16:27:40 +0900

    충돌이슈해결

--- src/main/java/kr/kh/final_project/dao/MemberDAO.java
+++ src/main/java/kr/kh/final_project/dao/MemberDAO.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import kr.kh.final_project.pagination.Criteria;
-import org.apache.ibatis.annotations.Param;
 
 import kr.kh.final_project.vo.MemberVO;
 
 public interface MemberDAO {
 
 	//List<MemberVO> selectMemberList(int me_num);
-	List<MemberVO> selectMemberList(@Param("cri")Criteria cri);
+	List<MemberVO> selectMemberListBySearch(@Param("cri")Criteria cri);
 
 	int selectTotalCount(@Param("cri")Criteria cri);
 	
