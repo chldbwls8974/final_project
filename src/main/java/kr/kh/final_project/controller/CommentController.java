6e52baa4385999fefc48900def5d85faf0c24333
sbk042 <sweetgirl042@naver.com>
2023-10-31 17:51:40 +0900

    댓글 기능구현중

--- src/main/java/kr/kh/final_project/controller/CommentController.java
+++ src/main/java/kr/kh/final_project/controller/CommentController.java
@@ -1,14 +1,18 @@
 package kr.kh.final_project.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.kh.final_project.pagination.Criteria;
+import kr.kh.final_project.pagination.PageMaker;
 import kr.kh.final_project.service.CommentService;
 import kr.kh.final_project.vo.CommentVO;
 
@@ -33,8 +37,21 @@ public class CommentController {
 	}
 	
 	// 댓글 조회하기
-
-	
+	// @RespenseBody : 직렬로 클라이언트로 전송하는 역할
+	@ResponseBody
+	@PostMapping("/comment/list/{bo_num}")
+	public Map<String, Object> list(@RequestBody Criteria cri, @PathVariable("bo_num")int bo_num){
+		Map<String, Object> map = new HashMap<String, Object>();
+		// 페이지당 항목수 5개로 잡음
+		cri.setPerPageNum(5);
+		List<CommentVO> list = commentService.getCommentList(bo_num, cri);
+		// 이 게시글의 총 댓글 갯수를 가져와라
+		int totalCount = commentService.getTotalCount(bo_num);
+		PageMaker pm = new PageMaker(3, cri, totalCount);
+		map.put("list", list);
+		map.put("pm", pm);
+		return map;
+	}
 }
 	
 
--- src/main/java/kr/kh/final_project/service/CommentService.java
+++ src/main/java/kr/kh/final_project/service/CommentService.java
@@ -1,10 +1,17 @@
 package kr.kh.final_project.service;
 
+import java.util.List;
+
+import kr.kh.final_project.pagination.Criteria;
 import kr.kh.final_project.vo.CommentVO;
 
 public interface CommentService {
 	
 	// 댓글등록하기
 	boolean insertComment(CommentVO comment);
+	// 등록된 댓글 리스트 가져오기
+	List<CommentVO> getCommentList(int bo_num, Criteria cri);
+	// 해당 게시글의 총 댓글 수 가져오기
+	int getTotalCount(int bo_num);
 
 }
