7a7c76cd99545e0dc9b9e4240baabf895baaf87b
ekzot1212 <ekzot1212@gmail.com>
2023-11-01 10:10:45 +0900

    병합수정

--- src/main/java/kr/kh/final_project/service/AdminService.java
+++ src/main/java/kr/kh/final_project/service/AdminService.java
@@ -35,7 +35,7 @@ public interface AdminService {
 	// 사업자신청 수락 버튼(권한 바꾸기)
 	boolean updateBusiness(ManagerVO manager);
 	// 사업자 정보 페이지네이션 (총 사업자신청 수 가져오기)
-	int getTotalCount3(Criteria cri);
+	int getTotalCount4(Criteria cri);
 	// 포인트 환급 관리 리스트를 검색결과로 가져오는 메서드
 	List<PointHistoryVO> getRefundListBySearch(Criteria cri, String searchType1, String searchType2);
 	// 포인트 환급 관리 리스트의 갯수를 가져오는 메서드
@@ -43,7 +43,6 @@ public interface AdminService {
 	//환급을 승인하는 메서드
 	boolean refundApproval(PointHistoryVO ph);
 	
-	int getTotalCount4(Criteria cri);
 	
 	// 사업자 정보조회
 	List<ManagerVO> getBusinessList2(Criteria cri);
