87fcce46ee2d10f97cf38c8c46f4333f386c4173
yimeunyoung <kil_yea@hanmail.net>
2023-11-06 11:44:57 +0900

    시설 : 페이지네이션/검색창 구현

--- src/main/java/kr/kh/final_project/dao/BusinessmanDAO.java
+++ src/main/java/kr/kh/final_project/dao/BusinessmanDAO.java
@@ -13,6 +13,8 @@ public interface BusinessmanDAO {
 
 	//시설 리스트 가져오기
 	List<FacilityVO> selectFacilityList(@Param("member")MemberVO member, @Param("cri")Criteria cri);
+	//현재 페이지 정보에 맞는 전체 시설 수 가져오기
+	int selectCountFacilityList(@Param("cri")Criteria cri, @Param("member")MemberVO member);
 	//회원번호로 사업자정보 가져오기
 	BusinessmanVO selectBusinessmanByMeNum(@Param("me_num")Integer me_num);
 	//시설 등록
@@ -21,8 +23,6 @@ public interface BusinessmanDAO {
 	boolean updateFacility(@Param("facility")FacilityVO facility);
 	//시설번호로 시설 정보 가져오기
 	FacilityVO selectFacility(@Param("fa_num")Integer fa_num);
-	//현재 페이지 정보에 맞는 전체 게시글 수 가져오기
-	int selectCountList(@Param("cri")Criteria cri, @Param("member")MemberVO member);
 
 
 }
