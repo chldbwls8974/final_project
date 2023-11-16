f61a7cf5b7895f232269bd2f894839563b6ab28e
ekzot1212 <ekzot1212@gmail.com>
2023-11-16 20:21:49 +0900

    .

--- src/main/java/kr/kh/final_project/interceptor/MemberInterceptor.java
+++ src/main/java/kr/kh/final_project/interceptor/MemberInterceptor.java
@@ -2,7 +2,9 @@ package kr.kh.final_project.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.kh.final_project.vo.MemberVO;
@@ -14,14 +16,25 @@ public class MemberInterceptor extends HandlerInterceptorAdapter{
 			HttpServletResponse response, Object handler)
 			throws Exception {
 		//user 권한이 없으면 로그인 페이지로 이동
-		MemberVO user= (MemberVO)request.getSession().getAttribute("user");
-		//로그인이 안되어 있으면
-		if(user == null) {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		//MemberVO user= (MemberVO)request.getSession().getAttribute("user");
+		
+		System.out.println("인터셉터 :"+ user);
+		//유저가 커뮤니티 정지 상태이면
+		if(user.getMe_state2() == 1) {
+			System.out.println("정지임");
 			//로그인 페이지로 이동
-			response.sendRedirect(request.getContextPath()+"/member/login");
+//			ModelAndView mv = new ModelAndView("redirect:/util/message");
+//			mv.addObject("url", "/");
+//			mv.addObject("msg", "왜 안되냐고");
+//			response.sendRedirect(request.getContextPath()+"/util/message");
+			request.setAttribute("msg", "되냐");
+            request.setAttribute("url", "/");
+            request.getRequestDispatcher("/util/message2").forward(request, response);
 			return false;
 		}
-		//로그인 했으면 기존에 가려던 URL을 방문해서 작업 
+		//정지가 아니라면 컨트롤러로 넘어감
 		return true;
 	}
 }
