6c3c8e1629a46e734e36a751336abf397ec8e24e
sbk042 <sweetgirl042@naver.com>
2023-11-16 18:21:28 +0900

    사업자 등록하기 (관리자)

--- src/main/java/kr/kh/final_project/dao/BusinessDAO.java
+++ src/main/java/kr/kh/final_project/dao/BusinessDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import kr.kh.final_project.pagination.Criteria;
+import kr.kh.final_project.vo.BusinessmanVO;
 import kr.kh.final_project.vo.ManagerVO;
 
 public interface BusinessDAO {
@@ -29,8 +30,19 @@ public interface BusinessDAO {
 
 	// 사업자 총 수 가져오기
 	int selectTotalCount2(@Param("cri")Criteria cri);
-	
 
+	// 사업자 관리자가 등록한 리스트 가져오기(3)
+	List<BusinessmanVO> selectBusinessListByInsert(@Param("cri")Criteria cri);
+	// 사업자 총 갯수 가져오기 (3)
+	int selectTotalCountByBusiness(@Param("cri")Criteria cri);
+	// 사업자 관리자가 등록하기 
+	boolean insertBusiness(@Param("bu")BusinessmanVO businessman);
+	// 수정할 사업자 가져오기 (3)
+	BusinessmanVO selectBusinessList3(@Param("bu_num")Integer bu_num);
+	// 사업자 수정하기 (3)
+	boolean updateBusiness(@Param("businessman")BusinessmanVO businessman);
+	// 사업자 삭제하기 (3)
+	boolean deleteBusinessList(@Param("bu_num")Integer bu_num);
 	
 	
 }
